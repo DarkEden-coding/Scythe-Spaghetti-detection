311d9e06acc087c3dfe3df3853e1a5ef40fe7042
DarkEden-coding <darkedenc9@gmail.com>
2023-09-18 10:29:44 -0400

    eee

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ async def on_ready():
                         file=discord.File(image_file),
                     )
 
-            boxes = detect(image, 0.1)
+            boxes = detect(image, 0.6)
             if boxes:
                 # pause()
 
