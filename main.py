ec7e007f95ef524c01a78509430a70cfdb756bd9
DarkEden-coding <darkedenc9@gmail.com>
2023-10-18 18:02:47 -0400

    eee

--- main.py
+++ main.py
@@ -16,83 +16,87 @@ token = "MTE0MTIwNDkxMzc3OTY1MDY0Mg.Gn-S0F.l5L2ixsATG53GgJR9uWabhbLWvYwTGlxVNOsE
 client = discord.Client(intents=intents)
 
 
-@client.event
-async def on_ready():
-    print(f'Logged in as {client.user.name}')
+try:
 
-    log_channel = client.get_channel(1139710420471525527)
-    counter = 0
+    @client.event
+    async def on_ready():
+        print(f'Logged in as {client.user.name}')
 
-    while True:
-        start_time = time()
+        log_channel = client.get_channel(1139710420471525527)
+        counter = 0
 
-        if counter % 3 == 0:
-            await log_channel.send(
-                embed=discord.Embed(
-                    title=f"Note: Monitoring for spaghetti. Has been running for {counter * 10} seconds.",
-                    color=discord.Color.green(),
-                )
-            )
+        while True:
+            start_time = time()
 
-        image = get_image()
-        if image:
-
-            image_path = 'fail_img.jpg'
-
-            if counter % 6 == 0:
-                with open('current_view.jpg', 'rb') as image_file:
-                    await log_channel.send(
-                        embed=discord.Embed(
-                            title=f"Note: Status, current camera image attached.",
-                            color=discord.Color.green(),
-                        ),
-                        file=discord.File(image_file),
+            if counter % 3 == 0:
+                await log_channel.send(
+                    embed=discord.Embed(
+                        title=f"Note: Monitoring for spaghetti. Has been running for {counter * 10} seconds.",
+                        color=discord.Color.green(),
                     )
-
-            boxes = detect(image, 0.6)
-            if boxes:
-                # pause()
-
-                with open(image_path, 'rb') as image_file:
-                    fail_message = await log_channel.send(
-                        embed=discord.Embed(
-                            title="FATAL: Spaghetti detected!",
-                            description="Print automatically paused.",
-                            color=discord.Color.red(),
-                        ),
-                        file=discord.File(image_file),
-                    )
-
-                await fail_message.add_reaction('👍')
-
-                message_id = fail_message.id
-
-                print("Waiting for reaction...")
-                loop = True
-                while loop:
-                    reaction_message = await log_channel.fetch_message(message_id)
-                    reactions = reaction_message.reactions
-
-                    for reaction in reactions:
-                        if reaction.count > 1:
-                            print("Reaction detected. Resuming detection.")
-                            loop = False
-                    await asyncio.sleep(1)
-
-        else:
-            await log_channel.send(
-                embed=discord.Embed(
-                    title="Error: Failed to get image.",
-                    color=discord.Color.orange(),
                 )
-            )
 
-        counter += 1
+            image = get_image()
+            if image:
 
-        end_time = time()
-        loop_time = end_time - start_time
+                image_path = 'fail_img.jpg'
 
-        await asyncio.sleep(10 - loop_time)
+                if counter % 6 == 0:
+                    with open('current_view.jpg', 'rb') as image_file:
+                        await log_channel.send(
+                            embed=discord.Embed(
+                                title=f"Note: Status, current camera image attached.",
+                                color=discord.Color.green(),
+                            ),
+                            file=discord.File(image_file),
+                        )
+
+                boxes = detect(image, 0.6)
+                if boxes:
+                    # pause()
+
+                    with open(image_path, 'rb') as image_file:
+                        fail_message = await log_channel.send(
+                            embed=discord.Embed(
+                                title="FATAL: Spaghetti detected!",
+                                description="Print automatically paused.",
+                                color=discord.Color.red(),
+                            ),
+                            file=discord.File(image_file),
+                        )
+
+                    await fail_message.add_reaction('👍')
+
+                    message_id = fail_message.id
+
+                    print("Waiting for reaction...")
+                    loop = True
+                    while loop:
+                        reaction_message = await log_channel.fetch_message(message_id)
+                        reactions = reaction_message.reactions
+
+                        for reaction in reactions:
+                            if reaction.count > 1:
+                                print("Reaction detected. Resuming detection.")
+                                loop = False
+                        await asyncio.sleep(1)
+
+            else:
+                await log_channel.send(
+                    embed=discord.Embed(
+                        title="Error: Failed to get image.",
+                        color=discord.Color.orange(),
+                    )
+                )
+
+            counter += 1
+
+            end_time = time()
+            loop_time = end_time - start_time
+
+            await asyncio.sleep(10 - loop_time)
 
 
-client.run(token)
+    client.run(token)
+except Exception as e:
+    print(f"Unexpected error has occurred: {e}")
--- test.py
+++ test.py
@@ -1,26 +1,46 @@
-import discord
+import requests
+import socket
+import time
 
-intents = discord.Intents.default()
-intents.typing = False
-intents.presences = False
-
-token = "MTE0MTIwNDkxMzc3OTY1MDY0Mg.Gn-S0F.l5L2ixsATG53GgJR9uWabhbLWvYwTGlxVNOsEc"
-client = discord.Client(intents=intents)
+# Discord webhook URL
+discord_webhook_url = 'https://discord.com/api/webhooks/1157127756124008490/zg-IhdWgMR7j-sXB4cGbYXlz0Fx0mvyXuUeIr9_-bybzi_XA5iLoPpNoNd1JjVvBe_cv'
 
 
-@client.event
-async def on_ready():
-    print(f'Logged in as {client.user.name}')
-
-    logs_channel = client.get_channel(1139710420471525527)
-
-    target_message = await logs_channel.fetch_message(1148427869782020116)  # Replace with the actual message ID
-
-    # Get the reactions on the message
-    reactions = target_message.reactions
-
-    print(reactions)
+# Function to get the current public IP address
+def get_public_ip():
+    try:
+        # Use a service like 'ipinfo.io' to get the public IP address
+        response = requests.get('https://ipinfo.io')
+        ip_data = response.json()
+        return ip_data['ip']
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
 
 
-client.run(token)
+# Function to send a message to Discord webhook
+def send_discord_message(old_ip, new_ip):
+    message = f"Public IP Address has changed!\n\nOld IP: {old_ip}\nNew IP: {new_ip}"
+    data = {
+        "content": message
+    }
+    response = requests.post(discord_webhook_url, json=data)
+    if response.status_code == 204:
+        print("Message sent successfully to Discord!")
+    else:
+        print(f"Failed to send message. Status code: {response.status_code}")
 
+
+# Initialize the old IP address
+old_ip = get_public_ip()
+
+while True:
+    new_ip = get_public_ip()
+    print(f"Old IP: {old_ip}")
+    print(f"New IP: {new_ip}")
+
+    if new_ip and old_ip != new_ip:
+        send_discord_message(old_ip, new_ip)
+        old_ip = new_ip
+
+    time.sleep(300)  # Sleep for 5 minutes (300 seconds)
