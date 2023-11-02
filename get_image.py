3b90bbeefd3682e20d31faf187b356acfdb45be1
DarkEden-coding <darkedenc9@gmail.com>
2023-11-02 16:46:53 -0400

    eee

--- get_image.py
+++ get_image.py
@@ -5,6 +5,7 @@ from io import BytesIO
 
 # URL of the image
 image_url = "http://mainsailos.local//webcam/?action=snapshot"
+ip_image_url = "http://10.0.0.212//webcam/?action=snapshot"
 
 
 def get_image():
@@ -12,7 +13,10 @@ def get_image():
     try:
         response = requests.get(image_url)
     except:
-        return False
+        try:
+            response = requests.get(ip_image_url)
+        except:
+            return False
 
     # Check if the request was successful
     if response.status_code == 200:
@@ -25,4 +29,16 @@ def get_image():
 
         return img
     else:
-        return False
+        response = requests.get(ip_image_url)
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Convert the image content to a PIL Image object
+            image_data = BytesIO(response.content)
+            img = Image.open(image_data)
+
+            # save the image to current_view.jpg
+            img.save("current_view.jpg")
+
+            return img
+        else:
+            return False
