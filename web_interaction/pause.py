0d21f9a8619c916fbbc4f71d6ff37de771cc804a
DarkEden-coding <darkedenc9@gmail.com>
2024-02-02 13:56:50 -0500

    remove webdriver-manager

--- web_interaction/pause.py
+++ web_interaction/pause.py
@@ -4,15 +4,23 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+import platform
 
 # Create ChromeOptions object and set headless mode
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 
-driver = webdriver.Chrome(
-    service=ChromeService(ChromeDriverManager().install()), options=chrome_options
-)
+if platform.system() == "Windows":
+    driver = webdriver.Chrome(
+        service=ChromeService(executable_path="../chromedriver_win32/chromedriver.exe"),
+        options=chrome_options,
+    )
+else:
+    driver = webdriver.Chrome(
+        service=ChromeService(executable_path="../chromedriver_linux64/chromedriver"),
+        options=chrome_options,
+    )
+
 wait = WebDriverWait(driver, 10)  # Wait up to 10 seconds
 
 
