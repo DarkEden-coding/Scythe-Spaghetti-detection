40fa3e3eaab8dd8aef49d45c0d7fb7ed34ece2cc
DarkEden-coding <darkedenc9@gmail.com>
2024-02-04 16:25:40 -0500

    change to use moonraker api to get webcam info, add option for selecting webcam for use

--- web_interaction/get_image.py
+++ web_interaction/get_image.py
@@ -1,13 +1,27 @@
 import requests
 from PIL import Image
 from io import BytesIO
-from settings import printer_url
+from settings import printer_url, webcam_name
+
+webcam_list = requests.get(printer_url + "server/webcams/list").json()["result"][
+    "webcams"
+]
+
+snapshot_url = None
+
+# Find the webcam URL
+for webcam in webcam_list:
+    if webcam["name"] == webcam_name:
+        snapshot_url = webcam["snapshot_url"]
+        break
+if not snapshot_url:
+    raise ValueError(f"Webcam '{webcam_name}' not found, the name is case sensitive")
 
 
 def get_image():
     # Fetch the image from the URL
     try:
-        response = requests.get(printer_url)
+        response = requests.get(printer_url + snapshot_url)
     except requests.exceptions.RequestException as e:
         print(f"Error fetching image: {e}")
         return False
