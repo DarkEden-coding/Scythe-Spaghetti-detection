344381c99ccbcf70040505c09c87c28584bd44b2
DarkEden-coding <darkedenc9@gmail.com>
2024-02-04 18:51:18 -0500

    small change to only allow pausing if the printer is printing

--- web_interaction/pause_print.py
+++ web_interaction/pause_print.py
@@ -1,6 +1,8 @@
 import requests
 from settings import printer_url
+from is_printing import is_printing
 
 
 def pause():
-    requests.post(printer_url + "printer/print/pause")
+    if is_printing():
+        requests.post(printer_url + "printer/print/pause")
