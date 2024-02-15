2119d9067022118ed9fb42ba95258efd1ed1c021
DarkEden-coding <darkedenc9@gmail.com>
2024-02-14 22:53:50 -0500

    add onnx_export.py

---
+++ onnx_export.py
@@ -0,0 +1,21 @@
+from ultralytics import YOLO
+
+
+def _util_export(path):
+    # Load a model
+    print("Loading model...")
+    model = YOLO(path)
+    print("Model loaded.")
+
+    # Export the model
+    print("Exporting model...")
+    model.export(
+        format="onnx", imgsz=640, simplify=True, device="cpu"
+    )  # settings for exporting to co processer
+    print("Model exported.")
+
+
+if __name__ == "__main__":
+    _util_export(
+        input("Enter the path of the model that you want to export (ending in .pt): ")
+    )
