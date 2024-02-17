ca40ed5e8c0e127e475c6435a207205186274a80
DarkEden-coding <darkedenc9@gmail.com>
2024-02-16 21:59:48 -0500

    make auto updates support updating main

--- update.py
+++ update.py
@@ -6,7 +6,10 @@ def check_for_updates():
         # git fetch
         use_sudo = False
         fetch_result = subprocess.run(["git", "fetch"], capture_output=True, text=True)
-        if "Permission denied" in fetch_result.stderr or "detected dubious ownership" in fetch_result.stderr:
+        if (
+            "Permission denied" in fetch_result.stderr
+            or "detected dubious ownership" in fetch_result.stderr
+        ):
             subprocess.run(
                 ["sudo", "git", "fetch"],
                 capture_output=True,
@@ -25,19 +28,19 @@ def check_for_updates():
         if "Your branch is behind" in status_result.stdout:
             print("\nThere are updates available. Auto Updating per settings...\n")
             print("Stashing local changes...")
-            subprocess.run(
-                ["sudo", "git", "stash"] if use_sudo else ["git", "stash"]
-            )
+            subprocess.run(["sudo", "git", "stash"] if use_sudo else ["git", "stash"])
 
             # Run 'git fetch' to fetch the latest changes from the remote repository
             print("Fetching latest changes...")
-            subprocess.run(
-                ["sudo", "git", "fetch"] if use_sudo else ["git", "fetch"]
-            )
+            subprocess.run(["sudo", "git", "fetch"] if use_sudo else ["git", "fetch"])
 
             # Run 'git pull' to pull the latest changes
             print("Pulling latest changes...")
-            pull_result = subprocess.run(["sudo", "git", "pull"] if use_sudo else ["git", "pull"], capture_output=True, text=True)
+            pull_result = subprocess.run(
+                ["sudo", "git", "pull"] if use_sudo else ["git", "pull"],
+                capture_output=True,
+                text=True,
+            )
 
             print(f"Pull result: {pull_result.stdout}")
 
