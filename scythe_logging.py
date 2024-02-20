a418c5b5338c29a017de22b6b38533d39f38ccf6
DarkEden-coding <darkedenc9@gmail.com>
2024-02-19 22:38:53 -0500

    add size limit of one gig to log file

--- scythe_logging.py
+++ scythe_logging.py
@@ -13,10 +13,18 @@ with open(log_file, "a") as file:
     file.write("-" * 100)
     file.write(f"\n{now.strftime('%Y-%m-%d %H:%M:%S')}\n")
     file.write("-" * 100)
-    file.write("\n")
+    file.write("\n\n")
 
 
 def log(message):
+
+    # if file is over one gigabyte remove the first 100 lines
+    if os.path.getsize(log_file) > 1000000000:
+        with open(log_file, "r") as file:
+            lines = file.readlines()
+        with open(log_file, "w") as file:
+            file.writelines(lines[100:])
+
     with open(log_file, "a") as file:
         file.write(f"{message}\n")
     print(message)
