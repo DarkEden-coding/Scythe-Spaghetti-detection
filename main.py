36ca62bb24f8c1f01076bbb0871880cba5da4d0e
DarkEden-coding <darkedenc9@gmail.com>
2024-03-13 18:14:59 -0400

    add printer status command to easily get printer status...

--- main.py
+++ main.py
@@ -238,4 +238,5 @@ async def get_printer_status(ctx):
     log("Sending printer status to user...")
     await ctx.response.send_message(f"Printer printing: {is_printing()}")
 
+
 client.run(discord_bot_token)
